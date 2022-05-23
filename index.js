34d2ed1402740fc43f255549a0ccdde5841ac3e5
Henrique Vieira <87788394+henriquecvieira@users.noreply.github.com>
2022-05-23 19:02:02 -0300

    INSERT

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 var database = require("./database")
 
 
-// INSERT
-// var dados = [
-//     {
-//     name: "Call of Duty of Thieves",
-//     price: 90.01
-//     },
-//     {
-//         name: "Minecraft",
-//         price: 56.06
-//     },
-//     {
-//         name: "resident evil",
-//         price: 75.03
-//     }
-// ]
+
 
 // database.insert(dados).into("games").then(data => {
 //     console.log(data)
@@ -24,8 +10,19 @@ var database = require("./database")
 // })
 
 //SELECT
-database.select().table("games").then(data => {
-    console.log(data)
-}).catch(err => {
-    console.log(err)
-})
+// database.select().table(["id", "price"]).table("games").then(data => {
+//     console.log(data)
+// }).catch(err => {
+//     console.log(err)
+// })
+
+//NESTED QUERIES
+// database.insert({name: "mists of moyah", price: 49}).into("games").then(data => {
+//     database.select().table(["id", "price"]).table("games").then(data => {
+//         console.log(data)
+//     }).catch(err => {
+//         console.log(err)
+//     })        
+// }).catch(err => {
+//     console.log(err)    
+// })
---
+++ insert.js
@@ -0,0 +1,15 @@
+INSERT
+var dados = [
+    {
+    name: "Call of Duty of Thieves",
+    price: 90.01
+    },
+    {
+        name: "Minecraft",
+        price: 56.06
+    },
+    {
+        name: "resident evil",
+        price: 75.03
+    }
+]
